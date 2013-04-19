fafe9245c99182c4960cbacf69386ea97a693d70
Vincent Pochet <vincent.pochet@gmail.com>
2013-04-19 13:53:00 +0200

    Base model and generators

---
+++ app/models/ip_to_country/geoip.rb
@@ -0,0 +1,32 @@
+# -*- encoding : utf-8 -*-
+
+module IpToCountry
+  class Geoip < ActiveRecord::Base
+
+    ###################
+    ### Validations ###
+    ###################
+
+    validates :ip_from_full, presence: true
+    validates :ip_to_full, presence: true
+    validates :ip_from, presence: true, uniqueness: true, numericality: :only_integer
+    validates :ip_to, presence: true, uniqueness: true, numericality: :only_integer
+    validates :country_code, presence: true
+    validates :country_name, presence: true
+
+    #####################
+    ### Class methods ###
+    #####################
+
+    def self.by_ip(ip)
+      where("ip_from = (select max(ip_from) from geoips where ip_from <= ?) and ip_to = (select min(ip_to)
+        from geoips where ip_to >= ?)", ip_to_integer(ip), ip_to_integer(ip)).first
+    end
+
+    def self.ip_to_integer(ip)
+      (o1, o2, o3, o4) = ip.split('.')
+      (16777216 * o1.to_i) + (65536 * o2.to_i) + (256 * o3.to_i) + o4.to_i
+    end
+
+  end
+end
---
+++ lib/generators/geoip/geoip_generator.rb
@@ -0,0 +1,23 @@
+# -*- encoding : utf-8 -*-
+require 'rails/generators'
+require 'rails/generators/migration'
+
+class GeoipGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+  
+  def self.source_root
+    @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+  end
+
+  def self.next_migration_number(dirname)
+    if ActiveRecord::Base.timestamped_migrations
+      Time.new.utc.strftime("%Y%m%d%H%M%S")
+    else
+      "%.3d" % (current_migration_number(dirname) + 1)
+    end
+  end
+
+  def create_migration_file
+    migration_template 'migration.rb', 'db/migrate/create_geoips_table.rb'
+  end
+end
