156a6463415b0924d35dba4a1e10dd341f9204c4
Vincent Pochet <vincent.pochet@gmail.com>
2013-04-19 22:40:12 +0200

    Exclude tests from covergage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,13 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+require "simplecov"
 require 'coveralls'
 require "mocha/setup"
+
+SimpleCov.start do
+  add_filter "/test/*"
+end
 
 Coveralls.wear!
 
